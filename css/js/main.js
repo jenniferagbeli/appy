717e8df4baf0ed9a3d9605f317a912c3a9b02d97
jenniferagbeli <agbelij12@gmail.com>
2024-02-12 22:02:23 +1200

    Work on appy navbar

---
+++ css/js/main.js
@@ -0,0 +1,32 @@
+var firstName = "Jennifer"; //firstname is a word
+var lastName = 'Agbeli';
+var age =18; //age is a number
+
+// let fullName = firstName+lastName;
+let fullName =  `${firstName} ${lastName}`;
+let a = 56;
+let b = 43;
+
+console.log(fullName);
+console.log(result)
+
+const boilingwaterTemperature = 100;
+
+let areYouHappy = true;
+
+let phone ={color:'black',makeCall:null};
+
+if(age==18)(console.log(`Welcome to XXX`))
+else{console.log('This page is not for you!')}
+
+for(let i = 1; 1 <=8; i++) {
+    console.log('The value if i is', i)
+}
+
+function isEligible() {
+    if(age >=18) { return'Welcome to XXX';}
+    else {return 'This page is not for you!';}
+}
+
+console.log(isEligible)
+document.body,innerHTML = isEligible()
