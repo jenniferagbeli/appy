94a9ee66fe46cda10b8f7d9e007bc8e2a8f2d06d
jenniferagbeli <agbelij12@gmail.com>
2024-02-12 22:20:08 +1200

    Added a line of code

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ var age =18; //age is a number
 let fullName =  `${firstName} ${lastName}`;
 let a = 56;
 let b = 43;
-let result = `${a} + `
 
 console.log(fullName);
 console.log(result)
@@ -15,9 +14,9 @@ const boilingwaterTemperature = 100;
 
 let areYouHappy = true;
 
-let phone ={color:'black', makeCall: null};
+let phone ={color:'black',makeCall:null};
 
-if(age==18)(console.log('Welcome to XXX'))
+if(age==18)(console.log(`Welcome to XXX`))
 else{console.log('This page is not for you!')}
 
 for(let i = 1; 1 <=8; i++) {
@@ -29,7 +28,5 @@ function isEligible() {
     else {return 'This page is not for you!';}
 }
 
-console.log(isEligible())
+console.log(isEligible)
 document.body,innerHTML = isEligible()
-
-console.log("We are using GIT!");
